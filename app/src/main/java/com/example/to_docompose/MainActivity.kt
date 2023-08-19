10d999f611e7937c4753a7b43eaebb7788aece57
Vinícius Thiengo <viniciusthiengo@gmail.com>
2023-08-19 20:47:58 -0300

    Removing unnecessary composable functions.

--- app/src/main/java/com/example/to_docompose/MainActivity.kt
+++ app/src/main/java/com/example/to_docompose/MainActivity.kt
@@ -3,13 +3,6 @@ package com.example.to_docompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.to_docompose.ui.theme.ToDoComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,18 +13,5 @@ class MainActivity : ComponentActivity() {
 
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ToDoComposeTheme {
-        Greeting("Android")
     }
 }
