ea0526f88c537d00849591d1e12600c2043deb27
Vinícius Thiengo <viniciusthiengo@gmail.com>
2023-08-24 12:05:46 -0300

    Adding new constants that are going to be part of the compose navigation strategy.

--- app/src/main/java/com/example/to_docompose/util/Constants.kt
+++ app/src/main/java/com/example/to_docompose/util/Constants.kt
@@ -3,4 +3,10 @@ package com.example.to_docompose.util
 object Constants {
     const val DATABASE_NAME = "todo_database"
     const val DATABASE_TABLE = "todo_table"
+
+    const val LIST_ARGUMENT_KEY = "action"
+    const val LIST_SCREEN = "list/{$LIST_ARGUMENT_KEY}"
+
+    const val TASK_ARGUMENT_KEY = "taskId"
+    const val TASK_SCREEN = "task/{$TASK_ARGUMENT_KEY}"
 }
