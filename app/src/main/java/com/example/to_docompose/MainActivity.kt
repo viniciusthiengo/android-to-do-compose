f13d54afa875db52b1bd31748431f703595a7e88
Vinícius Thiengo <viniciusthiengo@gmail.com>
2023-08-26 11:42:58 -0300

    Setting the new status bar composable also on the MainActivity.

--- app/src/main/java/com/example/to_docompose/MainActivity.kt
+++ app/src/main/java/com/example/to_docompose/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.to_docompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.to_docompose.navigation.SetupNavigation
+import com.example.to_docompose.ui.screens.SetStatusBarColor
 import com.example.to_docompose.ui.theme.ToDoComposeTheme
+import com.example.to_docompose.ui.theme.topAppBarBackgroundColor
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,6 +21,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ToDoComposeTheme {
+                SetStatusBarColor(color = MaterialTheme.colors.topAppBarBackgroundColor)
                 navController = rememberNavController()
                 SetupNavigation(navController = navController)
             }
