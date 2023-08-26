03fc0f507cb33cf12a35256f0f11701c24b86269
Vinícius Thiengo <viniciusthiengo@gmail.com>
2023-08-26 15:58:45 -0300

    Adding the new list top bar composable.

--- app/src/main/java/com/example/to_docompose/ui/screens/list/ListAppBar.kt
+++ app/src/main/java/com/example/to_docompose/ui/screens/list/ListAppBar.kt
@@ -1,23 +1,40 @@
 package com.example.to_docompose.ui.screens.list
 
 import android.content.res.Configuration
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.to_docompose.R
+import com.example.to_docompose.components.PriorityItem
+import com.example.to_docompose.data.models.Priority
+import com.example.to_docompose.ui.theme.LARGE_PADDING
+import com.example.to_docompose.ui.theme.Typography
 import com.example.to_docompose.ui.theme.topAppBarBackgroundColor
 import com.example.to_docompose.ui.theme.topAppBarContentColor
 
 @Composable
-fun ListAppBar() {
-    DefaultListAppBar()
+fun ListAppBar(modifier: Modifier = Modifier) {
+    DefaultListAppBar(
+        onSearchClicked = { },
+        onSortClicked = {},
+        onDeleteClicked = {},
+        modifier = modifier
+    )
 }
 
 @Composable
-fun DefaultListAppBar() {
+fun DefaultListAppBar(
+    onSearchClicked: () -> Unit,
+    onSortClicked: (Priority) -> Unit,
+    onDeleteClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     TopAppBar(
         title = {
             Text(
@@ -25,8 +42,146 @@ fun DefaultListAppBar() {
                 color = MaterialTheme.colors.topAppBarContentColor
             )
         },
-        backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor
+        actions = {
+            ListAppBarActions(
+                onSearchClicked = onSearchClicked,
+                onSortClicked = onSortClicked,
+                onDeleteClicked = onDeleteClicked
+            )
+        },
+        backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor,
+        modifier = modifier
     )
+}
+
+@Composable
+fun ListAppBarActions(
+    onSearchClicked: () -> Unit,
+    onSortClicked: (Priority) -> Unit,
+    onDeleteClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    SearchAction(
+        onSearchClicked = onSearchClicked,
+        modifier = modifier
+    )
+    SortAction(
+        onSortClicked = onSortClicked,
+        modifier = modifier
+    )
+    DeleteAction(
+        onDeleteClicked = onDeleteClicked,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun SearchAction(
+    onSearchClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    IconButton(
+        onClick = { onSearchClicked() },
+        modifier = modifier
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Search,
+            contentDescription = stringResource(R.string.search_tasks),
+            tint = MaterialTheme.colors.topAppBarContentColor
+        )
+    }
+}
+
+@Composable
+fun SortAction(
+    onSortClicked: (Priority) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var expanded by remember {
+        mutableStateOf(value = false)
+    }
+
+    IconButton(
+        onClick = { expanded = true },
+        modifier = modifier
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_filter_list),
+            contentDescription = stringResource(R.string.sort_tasks),
+            tint = MaterialTheme.colors.topAppBarContentColor
+        )
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                onClick = {
+                    expanded = false
+                    onSortClicked(Priority.LOW)
+                }
+            ) {
+                PriorityItem(priority = Priority.LOW)
+            }
+
+            DropdownMenuItem(
+                onClick = {
+                    expanded = false
+                    onSortClicked(Priority.HIGH)
+                }
+            ) {
+                PriorityItem(priority = Priority.HIGH)
+            }
+
+            DropdownMenuItem(
+                onClick = {
+                    expanded = false
+                    onSortClicked(Priority.NONE)
+                }
+            ) {
+                PriorityItem(priority = Priority.NONE)
+            }
+        }
+    }
+}
+
+@Composable
+fun DeleteAction(
+    onDeleteClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var expanded by remember {
+        mutableStateOf(value = false)
+    }
+
+    IconButton(
+        onClick = { expanded = true },
+        modifier = modifier
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_vertical_menu),
+            contentDescription = stringResource(R.string.delete_tasks),
+            tint = MaterialTheme.colors.topAppBarContentColor
+        )
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                onClick = {
+                    expanded = false
+                    onDeleteClicked()
+                }
+            ) {
+                Text(
+                    text = stringResource(R.string.delete_all),
+                    style = Typography.subtitle2,
+                    modifier = Modifier.padding(start = LARGE_PADDING)
+                )
+            }
+        }
+    }
 }
 
 @Composable
@@ -47,5 +202,9 @@ fun ListAppBarPreview() {
 @Composable
 @Preview
 fun DefaultListAppBarPreview() {
-    DefaultListAppBar()
+    DefaultListAppBar(
+        onSearchClicked = {},
+        onSortClicked = {},
+        onDeleteClicked = {}
+    )
 }
