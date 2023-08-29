ac0753ded58ff0afe4be4126bd6d48021b57426b
Vinícius Thiengo <viniciusthiengo@gmail.com>
2023-08-29 11:57:51 -0300

    Adding the some of the new task screen composable functions.

---
+++ app/src/main/java/com/example/to_docompose/ui/screens/task/TaskScreen.kt
@@ -0,0 +1,15 @@
+package com.example.to_docompose.ui.screens.task
+
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import com.example.to_docompose.util.Action
+
+@Composable
+fun TaskScreen(navigateToListScreen: (Action) -> Unit) {
+    Scaffold(
+        topBar = {
+            TaskAppBar(navigateToListScreen = navigateToListScreen)
+        },
+        content = {}
+    )
+}
