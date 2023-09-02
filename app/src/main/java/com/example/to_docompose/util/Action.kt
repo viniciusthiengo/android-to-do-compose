1b9395e7e7e9ee6a7e870df2c9da6e3159f9edad
Vinícius Thiengo <viniciusthiengo@gmail.com>
2023-09-02 11:40:48 -0300

    Adding an action String parser.

--- app/src/main/java/com/example/to_docompose/util/Action.kt
+++ app/src/main/java/com/example/to_docompose/util/Action.kt
@@ -8,3 +8,14 @@ enum class Action {
     UNDO,
     NO_ACTION
 }
+
+fun String?.toAction(): Action {
+    return when {
+        this.equals("ADD", true) -> Action.ADD
+        this.equals("UPDATE", true) -> Action.UPDATE
+        this.equals("DELETE", true) -> Action.DELETE
+        this.equals("DELETE_ALL", true) -> Action.DELETE_ALL
+        this.equals("UNDO", true) -> Action.UNDO
+        else -> Action.NO_ACTION
+    }
+}
