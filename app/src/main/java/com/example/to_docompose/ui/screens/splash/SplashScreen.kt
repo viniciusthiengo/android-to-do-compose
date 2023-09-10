1aac37baecdbae14cdb2282a707ec5703c72d7ba
Vinícius Thiengo <viniciusthiengo@gmail.com>
2023-09-10 12:04:27 -0300

    Adding the new SplashScreen composable and resources.

---
+++ app/src/main/java/com/example/to_docompose/ui/screens/splash/SplashScreen.kt
@@ -0,0 +1,63 @@
+package com.example.to_docompose.ui.screens.splash
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.to_docompose.R
+import com.example.to_docompose.ui.theme.LOGO_HEIGHT
+import com.example.to_docompose.ui.theme.ToDoComposeTheme
+import com.example.to_docompose.ui.theme.splashScreenBackground
+
+@Composable
+fun SplashScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colors.splashScreenBackground),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = getLogo()),
+            contentDescription = stringResource(R.string.todo_logo),
+            modifier = Modifier.size(LOGO_HEIGHT)
+        )
+    }
+}
+
+@Composable
+private fun getLogo(): Int =
+    if (isSystemInDarkTheme()) {
+        R.drawable.logo_dark
+    } else {
+        R.drawable.logo_light
+    }
+
+@Composable
+@Preview
+fun SplashScreenPreview() {
+    ToDoComposeTheme {
+        SplashScreen()
+    }
+}
+
+@Composable
+@Preview(
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+fun SplashScreenPreviewDark() {
+    ToDoComposeTheme {
+        SplashScreen()
+    }
+}
--- app/src/main/java/com/example/to_docompose/ui/theme/Color.kt
+++ app/src/main/java/com/example/to_docompose/ui/theme/Color.kt
@@ -37,3 +37,7 @@ val Colors.taskItemBackgroundColor: Color
 val Colors.taskItemTextColor: Color
     @Composable
     get() = if (isLight) DarkGray else LightGray
+
+val Colors.splashScreenBackground: Color
+    @Composable
+    get() = if (isLight) Purple700 else Color.Black
