862ffe98fd47cf366f8cc4f67518bc6ff594baa2
Vinícius Thiengo <viniciusthiengo@gmail.com>
2023-09-10 12:24:55 -0300

    Consuming the SplashScreen as the first app page to show.

--- app/src/main/java/com/example/to_docompose/navigation/Navigation.kt
+++ app/src/main/java/com/example/to_docompose/navigation/Navigation.kt
@@ -5,9 +5,10 @@ import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import com.example.to_docompose.navigation.destinations.listComposable
+import com.example.to_docompose.navigation.destinations.splashComposable
 import com.example.to_docompose.navigation.destinations.taskComposable
 import com.example.to_docompose.ui.viewmodels.SharedViewModel
-import com.example.to_docompose.util.Constants.LIST_SCREEN
+import com.example.to_docompose.util.Constants.SPLASH_SCREEN
 
 @Composable
 fun SetupNavigation(
@@ -20,14 +21,17 @@ fun SetupNavigation(
 
     NavHost(
         navController = navController,
-        startDestination = LIST_SCREEN
+        startDestination = SPLASH_SCREEN
     ) {
+        splashComposable(
+            navigateToTaskScreen = screen.splash
+        )
         listComposable(
-            navigateToTaskScreen = screen.task,
+            navigateToTaskScreen = screen.list,
             sharedViewModel = sharedViewModel
         )
         taskComposable(
-            navigateToListScreen = screen.list,
+            navigateToListScreen = screen.task,
             sharedViewModel = sharedViewModel
         )
     }
--- app/src/main/java/com/example/to_docompose/navigation/Screens.kt
+++ app/src/main/java/com/example/to_docompose/navigation/Screens.kt
@@ -3,17 +3,26 @@ package com.example.to_docompose.navigation
 import androidx.navigation.NavController
 import com.example.to_docompose.util.Action
 import com.example.to_docompose.util.Constants.LIST_SCREEN
+import com.example.to_docompose.util.Constants.SPLASH_SCREEN
 
 class Screens(navController: NavController) {
-    val list: (Action) -> Unit = { action ->
-        navController.navigate(route = "list/${action.name}") {
-            popUpTo(route = LIST_SCREEN) {
+    val splash: () -> Unit = {
+        navController.navigate(route = "list/${Action.NO_ACTION.name}") {
+            popUpTo(route = SPLASH_SCREEN) {
                 inclusive = true
             }
         }
     }
 
-    val task: (Int) -> Unit = { taskId ->
+    val list: (Int) -> Unit = { taskId ->
         navController.navigate(route = "task/$taskId")
+    }
+
+    val task: (Action) -> Unit = { action ->
+        navController.navigate(route = "list/${action.name}") {
+            popUpTo(route = LIST_SCREEN) {
+                inclusive = true
+            }
+        }
     }
 }
