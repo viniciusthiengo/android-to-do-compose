eb323c74dc6dd0a72e052c75d568410f34841544
Vinícius Thiengo <viniciusthiengo@gmail.com>
2023-09-10 12:33:35 -0300

    Adding the SplashScreen animation.

--- app/src/main/java/com/example/to_docompose/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/example/to_docompose/ui/screens/splash/SplashScreen.kt
@@ -1,20 +1,25 @@
 package com.example.to_docompose.ui.screens.splash
 
 import android.content.res.Configuration
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.to_docompose.R
 import com.example.to_docompose.ui.theme.LOGO_HEIGHT
 import com.example.to_docompose.ui.theme.ToDoComposeTheme
@@ -27,9 +32,26 @@ fun SplashScreen(
     navigateToTaskScreen: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    var startAnimation by remember {
+        mutableStateOf(false)
+    }
+    val offsetState by animateDpAsState(
+        targetValue = if (startAnimation) 0.dp else 100.dp,
+        animationSpec = tween(
+            durationMillis = 1000
+        )
+    )
+    val alphaState by animateFloatAsState(
+        targetValue = if (startAnimation) 1f else 0f,
+        animationSpec = tween(
+            durationMillis = 1000
+        )
+    )
+
     LaunchedEffect(
         key1 = true,
         block = {
+            startAnimation = true
             delay(timeMillis = SPLASH_SCREEN_DELAY)
             navigateToTaskScreen()
         }
@@ -44,7 +66,10 @@ fun SplashScreen(
         Image(
             painter = painterResource(id = getLogo()),
             contentDescription = stringResource(R.string.todo_logo),
-            modifier = Modifier.size(LOGO_HEIGHT)
+            modifier = Modifier
+                .size(LOGO_HEIGHT)
+                .offset(y = offsetState)
+                .alpha(alpha = alphaState)
         )
     }
 }
