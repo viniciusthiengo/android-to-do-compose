17855494a50428178ffd7da0d000a4b6d19e6bcf
Vinícius Thiengo <viniciusthiengo@gmail.com>
2023-09-12 11:21:06 -0300

    Code refactoring on DropDown, mainly removing the unnecessary code.

--- app/src/main/java/com/example/to_docompose/components/PriorityDropDown.kt
+++ app/src/main/java/com/example/to_docompose/components/PriorityDropDown.kt
@@ -5,10 +5,7 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
@@ -17,8 +14,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import com.example.to_docompose.R
 import com.example.to_docompose.data.models.Priority
@@ -38,9 +38,16 @@ fun PriorityDropDown(
         targetValue = if (expanded) 180f else 0f
     )
 
+    var parentSize by remember {
+        mutableStateOf(IntSize.Zero)
+    }
+
     Row(
         modifier = modifier
             .fillMaxWidth()
+            .onGloballyPositioned {
+                parentSize = it.size
+            }
             .background(color = MaterialTheme.colors.background)
             .height(PRIORITY_DROP_DOWN_HEIGHT)
             .clickable { expanded = true }
@@ -80,32 +87,21 @@ fun PriorityDropDown(
         DropdownMenu(
             expanded = expanded,
             onDismissRequest = { expanded = false },
-            modifier = Modifier.fillMaxWidth(fraction = 0.94f)
+            modifier = Modifier.width(with(LocalDensity.current) { parentSize.width.toDp() })
         ) {
-            DropdownMenuItem(
-                onClick = {
-                    expanded = false
-                    onPrioritySelected(Priority.LOW)
+            Priority
+                .values()
+                .slice(0..2)
+                .forEach { priority ->
+                    DropdownMenuItem(
+                        onClick = {
+                            expanded = false
+                            onPrioritySelected(priority)
+                        }
+                    ) {
+                        PriorityItem(priority = priority)
+                    }
                 }
-            ) {
-                PriorityItem(priority = Priority.LOW)
-            }
-            DropdownMenuItem(
-                onClick = {
-                    expanded = false
-                    onPrioritySelected(Priority.MEDIUM)
-                }
-            ) {
-                PriorityItem(priority = Priority.MEDIUM)
-            }
-            DropdownMenuItem(
-                onClick = {
-                    expanded = false
-                    onPrioritySelected(Priority.HIGH)
-                }
-            ) {
-                PriorityItem(priority = Priority.HIGH)
-            }
         }
     }
 }
