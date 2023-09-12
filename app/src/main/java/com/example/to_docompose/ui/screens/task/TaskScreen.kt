fe121356be56246483cffefed38276ff90c77c1f
Vinícius Thiengo <viniciusthiengo@gmail.com>
2023-09-12 11:03:56 -0300

    Code style refactoring, removing unnecessary BackHandler code and using the native one.

--- app/src/main/java/com/example/to_docompose/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/com/example/to_docompose/ui/screens/task/TaskScreen.kt
@@ -2,9 +2,7 @@ package com.example.to_docompose.ui.screens.task
 
 import android.content.Context
 import android.widget.Toast
-import androidx.activity.OnBackPressedCallback
-import androidx.activity.OnBackPressedDispatcher
-import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
+import androidx.activity.compose.BackHandler
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalContext
@@ -24,7 +22,9 @@ fun TaskScreen(
     val priority: Priority by sharedViewModel.priority
     val context = LocalContext.current
 
-    BackHandler(onBackPressed = { navigateToListScreen(Action.NO_ACTION) })
+    BackHandler {
+        navigateToListScreen(Action.NO_ACTION)
+    }
 
     Scaffold(
         topBar = {
@@ -70,31 +70,4 @@ fun displayInvalidFieldsToast(context: Context) {
             Toast.LENGTH_SHORT
         )
         .show()
-}
-
-@Composable
-fun BackHandler(
-    backDispatcher: OnBackPressedDispatcher? = LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher,
-    onBackPressed: () -> Unit
-) {
-    val currentOnBackPressed by rememberUpdatedState(newValue = onBackPressed)
-
-    val backCallback = remember {
-        object : OnBackPressedCallback(enabled = true) {
-            override fun handleOnBackPressed() {
-                currentOnBackPressed()
-            }
-        }
-    }
-
-    DisposableEffect(
-        key1 = backDispatcher,
-        effect = {
-            backDispatcher?.addCallback(backCallback)
-
-            onDispose {
-                backCallback.remove()
-            }
-        }
-    )
 }
