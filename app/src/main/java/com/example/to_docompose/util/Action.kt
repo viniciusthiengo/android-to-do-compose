c497f67da1aa5e1fdb31011779caeff519b14a6c
Vinícius Thiengo <viniciusthiengo@gmail.com>
2023-09-12 11:29:08 -0300

    Code design refactoring.

--- app/src/main/java/com/example/to_docompose/util/Action.kt
+++ app/src/main/java/com/example/to_docompose/util/Action.kt
@@ -9,13 +9,9 @@ enum class Action {
     NO_ACTION
 }
 
-fun String?.toAction(): Action {
-    return when {
-        this.equals("ADD", true) -> Action.ADD
-        this.equals("UPDATE", true) -> Action.UPDATE
-        this.equals("DELETE", true) -> Action.DELETE
-        this.equals("DELETE_ALL", true) -> Action.DELETE_ALL
-        this.equals("UNDO", true) -> Action.UNDO
-        else -> Action.NO_ACTION
+fun String?.toAction(): Action =
+    if (this.isNullOrEmpty()) {
+        Action.NO_ACTION
+    } else {
+        Action.valueOf(this.uppercase())
     }
-}
